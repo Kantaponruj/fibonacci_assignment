ad77b0512de2156c99f4ffae165fa9282c300d94
Kantapon Rujijanakul <kantaponruj@gmail.com>
2025-02-08 13:40:40 +0700

    Remove unused code

--- lib/utils/themes/custom_theme.dart
+++ lib/utils/themes/custom_theme.dart
@@ -5,22 +5,3 @@ const redBackgroundColor = Color(0xffFFA194);
 const greenBackgroundColor = Color(0xffB6E8A1);
 
 const primaryColor = Color(0xffFF924D);
-
-ThemeData customTheme() {
-  return ThemeData(
-    useMaterial3: false,
-    // fontFamily: 'IBM',
-    // primarySwatch: Colors.blue,
-    // scaffoldBackgroundColor: backgroundColor,
-    // appBarTheme: _buildAppBarTheme(),
-  );
-}
-
-// AppBarTheme _buildAppBarTheme() {
-//   return AppBarTheme(
-//     backgroundColor: backgroundColor,
-//     elevation: 0,
-//     iconTheme: IconThemeData(color: Colors.black),
-//     titleTextStyle: headingText().copyWith(color: primaryColor),
-//   );
-// }
